8793c8d65d71747ada8173ae9e323e8d5fe18244
rcslbaa <ifasmarani3125@gmail.com>
2025-12-19 20:23:27 +0700

    menambahkan HomeViewModel

--- app/src/main/java/com/example/questapi_015/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_015/viewmodel/HomeViewModel.kt
@@ -1,2 +1,40 @@
 package com.example.questapi_015.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_015.modeldata.DataSiswa
+import com.example.questapi_015.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val Siswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
