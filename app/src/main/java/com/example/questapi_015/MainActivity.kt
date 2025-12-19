1cb13e47b89156a9a34e8e23db265a2b5fa3ba3d
rcslbaa <ifasmarani3125@gmail.com>
2025-12-19 20:54:28 +0700

    menambahkan MainActivity

--- app/src/main/java/com/example/questapi_015/MainActivity.kt
+++ app/src/main/java/com/example/questapi_015/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_015.ui.theme.QuestAPI_015Theme
+import com.example.questapi_015.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAPI_015Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_015Theme {
-        Greeting("Android")
     }
 }
