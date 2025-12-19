c7b4606c9c5078b6f27f1a46af147114f97eb2c0
rcslbaa <ifasmarani3125@gmail.com>
2025-12-19 21:46:36 +0700

    menambahkan DestinasiDetail

--- app/src/main/java/com/example/questapi_015/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_015/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_015.uicontroller.route
 
+import com.example.questapi_015.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+    const val SISWA_ID = "itemId"
+    val routeWithArgs = "$route/{$SISWA_ID}"
+}
