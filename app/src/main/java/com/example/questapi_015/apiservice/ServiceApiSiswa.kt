3398c1fe92e666f88bdfd1a2f2b032e22ebf7ada
rcslbaa <ifasmarani3125@gmail.com>
2025-12-19 18:19:33 +0700

    menambahkan ServiceApiSiswa

--- app/src/main/java/com/example/questapi_015/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_015/apiservice/ServiceApiSiswa.kt
@@ -1,4 +1,14 @@
 package com.example.questapi_015.apiservice
 
+import com.example.questapi_015.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
