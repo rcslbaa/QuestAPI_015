e94d3579686348efd04dfc8ef5c8162cdc3739b8
rcslbaa <ifasmarani3125@gmail.com>
2025-12-19 19:30:10 +0700

    menambahkan DestinasiEntry

--- app/src/main/java/com/example/questapi_015/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/questapi_015/uicontroller/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.questapi_015.uicontroller.route
 
-object DestinasiEntry {
+import com.example.questapi_015.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
