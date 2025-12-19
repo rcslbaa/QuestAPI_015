81675a7f9a86e44f75cf82b5674e5d75f126eb92
rcslbaa <ifasmarani3125@gmail.com>
2025-12-19 18:49:04 +0700

    menambahkan RepositoryDataSiswa

--- app/src/main/java/com/example/questapi_015/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_015/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,17 @@
 package com.example.questapi_015.repositori
 
+import com.example.questapi_015.apiservice.ServiceApiSiswa
+import com.example.questapi_015.modeldata.DataSiswa
+import retrofit2.Response
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
