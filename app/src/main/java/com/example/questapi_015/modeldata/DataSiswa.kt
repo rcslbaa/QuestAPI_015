d464c3b2a2aa0a56cd04db12f595617bc7a8ab2d
rcslbaa <ifasmarani3125@gmail.com>
2025-12-19 18:14:09 +0700

    menambahkan DataSiswa

--- app/src/main/java/com/example/questapi_015/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi_015/modeldata/DataSiswa.kt
@@ -1,2 +1,41 @@
 package com.example.questapi_015.modeldata
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id : Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
