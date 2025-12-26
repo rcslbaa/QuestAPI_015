a774411265dc99d5ccef7507590024a1819c37bd
rcslbaa <ifasmarani3125@gmail.com>
2025-12-27 04:42:45 +0700

    menampilkan satu baris detail data

--- app/src/main/java/com/example/questapi_015/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_015/view/HalamanDetail.kt
@@ -181,3 +181,16 @@ fun DetailDataSiswa(
     }
 }
 
+@Composable
+private fun BarisDetailData(
+    @StringRes labelResID: Int,
+    itemDetail: String,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
+
