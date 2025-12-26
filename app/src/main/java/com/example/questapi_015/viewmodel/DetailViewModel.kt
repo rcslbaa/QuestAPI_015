5e41ed1c039e8211bbfde2e058bda66e22f8ea1e
rcslbaa <ifasmarani3125@gmail.com>
2025-12-27 04:48:23 +0700

    mengelola data detail siswa berdasarkan ID

--- app/src/main/java/com/example/questapi_015/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_015/viewmodel/DetailViewModel.kt
@@ -21,3 +21,33 @@ sealed interface StatusUiDetail {
     object Loading : StatusUiDetail
 }
 
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    var statusUiDetail: StatusUiDetail by mutableStateOf(StatusUiDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUiDetail = StatusUiDetail.Loading
+            statusUiDetail = try {
+                StatusUiDetail.Success(
+                    satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                )
+            } catch (e: IOException) {
+                StatusUiDetail.Error
+            } catch (e: HttpException) {
+                StatusUiDetail.Error
+            }
+        }
+    }
+
