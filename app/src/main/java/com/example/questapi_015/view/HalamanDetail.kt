e0eea78964bd89ce22dfbade0b6b7623f3516434
rcslbaa <ifasmarani3125@gmail.com>
2025-12-27 04:36:55 +0700

    menampilkan detail siswa sebagai konten utama layar

--- app/src/main/java/com/example/questapi_015/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_015/view/HalamanDetail.kt
@@ -77,4 +77,20 @@ fun DetailSiswaScreen(
             }
         },
         modifier = modifier
-    )
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            statusUiDetail = viewModel.statusUiDetail,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.hapusSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
