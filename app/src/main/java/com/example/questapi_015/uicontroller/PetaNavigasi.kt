194f411bf93ac3f8af2558fbbb052e2e3695ca94
rcslbaa <ifasmarani3125@gmail.com>
2025-12-27 05:00:34 +0700

    mendaftarkan halaman Edit Data Siswa

--- app/src/main/java/com/example/questapi_015/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_015/uicontroller/PetaNavigasi.kt
@@ -58,3 +58,11 @@ fun HostNavigasi(
                 navigateBack = { navController.navigate(DestinasiHome.route) })
         }
 
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg){
+            type= NavType.IntType})){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
+        }
+
+    }
+}
