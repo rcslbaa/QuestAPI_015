b72bc262ea268d793efceffe396faa92d942582f
rcslbaa <ifasmarani3125@gmail.com>
2025-12-27 04:47:31 +0700

    menampilkan status tampilan (UI state) pada halaman detail siswa

--- app/src/main/java/com/example/questapi_015/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_015/viewmodel/DetailViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.questapi_015.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_015.modeldata.DataSiswa
+import com.example.questapi_015.repositori.RepositoryDataSiswa
+import com.example.questapi_015.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUiDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUiDetail
+    object Error : StatusUiDetail
+    object Loading : StatusUiDetail
+}
+
