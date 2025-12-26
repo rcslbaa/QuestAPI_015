9c4304ebfedb4376387c986461174ebde72b78bd
rcslbaa <ifasmarani3125@gmail.com>
2025-12-27 04:45:10 +0700

    memperbarui DestinasiDetail

--- app/src/main/java/com/example/questapi_015/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_015/uicontroller/route/DestinasiDetail.kt
@@ -5,6 +5,6 @@ import com.example.questapi_015.R
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
-    const val SISWA_ID = "itemId"
-    val routeWithArgs = "$route/{$SISWA_ID}"
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
