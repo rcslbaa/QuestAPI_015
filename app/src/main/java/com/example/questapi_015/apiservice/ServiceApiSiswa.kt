7f0c12a68bf1ddf035bd48ac1d2ba45ccd1843ce
rcslbaa <ifasmarani3125@gmail.com>
2025-12-26 20:55:09 +0700

    menambahkan put

--- app/src/main/java/com/example/questapi_015/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_015/apiservice/ServiceApiSiswa.kt
@@ -18,3 +18,6 @@ interface ServiceApiSiswa {
     @GET("baca1Teman.php/{id}")
     suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
