521462d3ab0dee1f2f622cd4b9330c248a0eb00a
rcslbaa <ifasmarani3125@gmail.com>
2025-12-27 05:03:00 +0700

    memperbarui PenyediaViewModel

--- app/src/main/java/com/example/questapi_015/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_015/viewmodel/provider/PenyediaViewModel.kt
@@ -1,22 +1,42 @@
 package com.example.questapi_015.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_015.repositori.AplikasiDataSiswa
+import com.example.questapi_015.viewmodel.DetailViewModel
+import com.example.questapi_015.viewmodel.EditViewModel
 import com.example.questapi_015.viewmodel.EntryViewModel
 import com.example.questapi_015.viewmodel.HomeViewModel
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-                AplikasiDataSiswa
-        )
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as AplikasiDataSiswa)
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container
-            .repositoryDataSiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container
-            .repositoryDataSiswa) }
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
